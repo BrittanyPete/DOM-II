1e3168f6e2a135951b832df3b4d7796322e10bb2
Brittany Petersen <britt.petersen100@gmail.com>
2021-11-30 12:35:22 -0700

    created variables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './less/index.less'
 
 // Your code goes here!
+const logoHeading = document.querySelector('h1.logo-heading');
+const navLinks = document.querySelectorAll('nav a');
+const topImg = document.querySelector('header img');
+const destImg = document.querySelector('.content-destination img');
+const button = document.querySelectorAll('.btn');
+const destinations = document.querySelectorAll('.destination h4');
+const infoSection = document.querySelectorAll('.content-section h2');
+
+
